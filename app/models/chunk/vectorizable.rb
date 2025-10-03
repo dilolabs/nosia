572c084b96d1588c76d98c25773bfb6f242fafb9
Cyril Blaecke <cyril@dilolabs.fr>
2025-10-03 23:26:40 +0200

    Clean env vars

--- app/models/chunk/vectorizable.rb
+++ app/models/chunk/vectorizable.rb
@@ -8,8 +8,8 @@ module Chunk::Vectorizable
 
     scope :search_by_similarity, ->(query_text, limit: ENV["RETRIEVAL_FETCH_K"] || 5) {
       context = RubyLLM.context do |config|
-        config.openai_api_base = ENV["OPENAI_EMBEDDING_API_BASE"] || ENV["OPENAI_API_BASE"]
-        config.openai_api_key = ENV["OPENAI_API_KEY"]
+        config.openai_api_base = ENV["EMBEDDING_BASE_URL"] || ENV["AI_BASE_URL"]
+        config.openai_api_key = ENV["AI_API_KEY"]
       end
       query_embedding = RubyLLM.embed(query_text, context:, model: ENV["EMBEDDING_MODEL"], dimensions: ENV["EMBEDDING_DIMENSIONS"].to_i, provider: :openai, assume_model_exists: true).vectors
       nearest_neighbors(:embedding, query_embedding, distance: :cosine).limit(limit)
@@ -23,8 +23,8 @@ module Chunk::Vectorizable
     Rails.logger.info "Generating embedding for Chunk #{id}..."
     begin
       context = RubyLLM.context do |config|
-        config.openai_api_base = ENV["OPENAI_EMBEDDING_API_BASE"] || ENV["OPENAI_API_BASE"]
-        config.openai_api_key = ENV["OPENAI_API_KEY"]
+        config.openai_api_base = ENV["EMBEDDING_BASE_URL"] || ENV["AI_BASE_URL"]
+        config.openai_api_key = ENV["AI_API_KEY"]
       end
       embedding_result = RubyLLM.embed(content, context:, model: ENV["EMBEDDING_MODEL"], dimensions: ENV["EMBEDDING_DIMENSIONS"].to_i, provider: :openai, assume_model_exists: true) # Uses default embedding model
       self.embedding = embedding_result.vectors
