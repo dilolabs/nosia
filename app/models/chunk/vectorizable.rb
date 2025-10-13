8b19f1521a1c1599eb9f8dbab4cf55bf99c52e3e
Cyril Blaecke <1692273+cbldev@users.noreply.github.com>
2025-10-13 15:45:00 +0000

    Add searchable concern

--- app/models/chunk/vectorizable.rb
+++ app/models/chunk/vectorizable.rb
@@ -2,18 +2,7 @@ module Chunk::Vectorizable
   extend ActiveSupport::Concern
 
   included do
-    has_neighbors :embedding
-
     before_save :generate_embedding, if: :content_changed?
-
-    scope :search_by_similarity, ->(query_text, limit: ENV["RETRIEVAL_FETCH_K"].to_i || 5) {
-      context = RubyLLM.context do |config|
-        config.openai_api_base = ENV["EMBEDDING_BASE_URL"] || ENV["AI_BASE_URL"]
-        config.openai_api_key = ENV["AI_API_KEY"]
-      end
-      query_embedding = RubyLLM.embed(query_text, context:, model: ENV["EMBEDDING_MODEL"], dimensions: ENV["EMBEDDING_DIMENSIONS"].to_i, provider: :openai, assume_model_exists: true).vectors
-      nearest_neighbors(:embedding, query_embedding, distance: :cosine).limit(limit)
-    }
   end
 
   def generate_embedding
