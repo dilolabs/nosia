19f9fe94b786a4653e45af78deba6f1295e3d02f
Cyril Blaecke <1692273+cbldev@users.noreply.github.com>
2025-11-03 09:01:47 +0000

    Remove PDF metadata from PDF Reader

--- app/models/document/parsable.rb
+++ app/models/document/parsable.rb
@@ -21,7 +21,6 @@ module Document::Parsable
   def parse_pdf
     self.file.blob.open do |io|
       reader = ::PDF::Reader.new(io)
-      self.metadata = reader.metadata
       self.content = reader.pages.map(&:text).join("\n\n")
     end
   end
