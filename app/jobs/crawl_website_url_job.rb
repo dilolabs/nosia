39b50d84a34927a45a5286485a2e3e96a40da9a0
Cyril Blaecke <cyril@dilolabs.fr>
2026-07-07 19:02:16 +0200

    feat: retry CrawlWebsiteUrlJob on transient fetch errors

--- app/jobs/crawl_website_url_job.rb
+++ app/jobs/crawl_website_url_job.rb
@@ -1,6 +1,12 @@
 class CrawlWebsiteUrlJob < ApplicationJob
   queue_as :background
 
+  retry_on Faraday::TimeoutError,
+           Faraday::ConnectionFailed,
+           Faraday::ServerError,
+           wait: 30.seconds,
+           attempts: 5
+
   def perform(website_id)
     website = Website.find(website_id)
     website.crawl_url!
